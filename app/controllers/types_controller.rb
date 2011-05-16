d9addddfb8626862e2dc18cadcb14db40762d7f9
Andrea Reginato <andrea.reginato@gmail.com>
2011-05-16 12:32:44 +0200

    Added type index and show functionality with acceptance tests

---
+++ app/controllers/types_controller.rb
@@ -0,0 +1,57 @@
+class TypesController < ApplicationController
+  before_filter :parse_json_body, only: %w(create update)
+  before_filter :find_owned_resources
+  before_filter :find_resource, only: %w(show update destroy)
+  before_filter :find_connections, only: %w(show update destroy)
+
+  def index
+    @types = @types.page(params[:page]).per(params[:per])
+  end
+
+  def show
+  end
+
+  def create
+    @type = Type.base(json_body, request, current_user)
+    if @type.save
+      #create_default_status(@type)
+      render 'show', status: 201, location: @type.uri
+    else
+      render_422 'notifications.document.not_valid', @type.errors
+    end
+  end
+
+  def update
+    if @type.update_attributes(json_body)
+      render 'show'
+    else
+      render_422 'notifications.document.not_valid', @type.errors
+    end
+  end
+
+  def destroy
+    @type.destroy
+    render 'show'
+  end
+
+
+  private
+
+    def find_owned_resources
+      @types = Type.where(created_from: current_user.uri)
+    end
+
+    def find_resource
+      @type = @types.find(params[:id])
+    end
+
+    def find_connections
+      @properties = Property.any_in(uri: @type.properties)
+      @functions = Function.any_in(uri: @type.functions)
+    end
+
+    def create_default_status(type)
+      #@status = Status.base_default({name: "Default"}, request, current_user)
+      #type.type_statuses.create(uri: @status.uri)
+    end
+end
--- spec/factories/property.rb
+++ spec/factories/property.rb
@@ -14,4 +14,12 @@ FactoryGirl.define do
     created_from Settings.another_user.uri
     uri Settings.properties.intensity.uri + 'not_owned'
   end
+
+  factory :property_status, class: :property do
+    uri Settings.properties.status.uri
+    created_from Settings.user.uri
+    name Settings.properties.status.name
+    default Settings.properties.status.default_value
+    values Settings.properties.status.values
+  end
 end
