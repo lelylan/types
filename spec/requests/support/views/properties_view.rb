6111e66c4d27dfa5c8d33047bc1e5e4671120499
Andrea Reginato <andrea.reginato@gmail.com>
2012-07-06 11:27:18 +0200

    Created methods to check property view

--- spec/requests/support/views/properties_view.rb
+++ spec/requests/support/views/properties_view.rb
@@ -1,44 +1,39 @@
-module DevicesViewMethods
+module PropertiesViewMethods
 
-  def should_have_only_owned_device(device)
-    device = DeviceDecorator.decorate(device)
+  def should_have_owned_property(property)
+    property = PropertyDecorator.decorate(property)
     json = JSON.parse(page.source)
-    should_contain_device(device)
-    should_not_have_not_owned_devices
+    should_contain_property(property)
+    should_not_have_not_owned_properties
   end
 
-  def should_contain_device(device)
-    device = DeviceDecorator.decorate(device)
+  def should_contain_property(property)
+    property = PropertyDecorator.decorate(property)
     json = JSON.parse(page.source).first
-    should_have_device(device, json)
+    should_have_property(property, json)
   end
 
-  def should_have_device(device, json = nil)
-    device = DeviceDecorator.decorate(device)
+  def should_have_property(property, json = nil)
+    property = PropertyDecorator.decorate(property)
     should_have_valid_json
     json = JSON.parse(page.source) unless json 
     json = Hashie::Mash.new json
-    json.uri.should == device.uri
-    json.id.should == device.id.as_json
-    json.name.should == device.name
-    json.type.uri.should == device.type_uri
-    json.properties.each_with_index do |property, index|
-      property.uri.should == device.device_properties[index].uri
-      property.value.should == device.device_properties[index].value
-    end
-    json.physical.uri.should == device.device_physical.uri if device.device_physical
-    json.physical.should be_empty if !device.device_physical
-    json.pending.uri.should == device.uri + '/pending'
-    json.pending.status.should == device.pending
+    json.uri.should == property.uri
+    json.id.should == property.id.as_json
+    json.name.should == property.name
+    json.default.should == property.default
+    json.values.should == property.values
+    json.created_at.should == property.created_at
+    json.updated_at.should == property.updated_at
   end
 
-  def should_not_have_not_owned_devices
+  def should_not_have_not_owned_properties
     should_have_valid_json
     json = JSON.parse(page.source)
     json.should have(1).item
-    Device.all.should have(2).items
+    Property.all.should have(2).items
   end
 
 end
 
-RSpec.configuration.include DevicesViewMethods
+RSpec.configuration.include PropertiesViewMethods
