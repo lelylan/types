9f43f989bea05859d3e43be049dc6514269b7bf6
Andrea Reginato <andrea.reginato@gmail.com>
2012-07-06 11:51:54 +0200

    Refactoring property factories to follow standard structure

--- spec/factories/property.rb
+++ spec/factories/property.rb
@@ -5,21 +5,20 @@ FactoryGirl.define do
 
   # Property
   factory :property do
+    name 'Status'
     created_from Settings.user.uri
+    default Settings.properties.status.default
+    values Settings.properties.status.values
   end
 
   # Property not owned
-  factory :property_not_owned do
+  factory :property_not_owned, parent: :property do
     created_from Settings.user.another.uri
   end 
 
   # Status property
-  factory :status, parent: :property do
-    name 'Status'
-    default Settings.properties.status.default
-    values Settings.properties.status.values
-  end
-  
+  factory :status, parent: :property
+
   # Intensity property
   factory :intensity, parent: :property do
     name 'Intensity'
