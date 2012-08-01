73a59ec387fcca2f60ad8dbb0c2d0535b41f8a55
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-01 11:46:18 +0200

    Cleanup code FunctionProperty

--- app/models/function_property.rb
+++ app/models/function_property.rb
@@ -6,18 +6,15 @@ class FunctionProperty
   field :property_id
 
   attr_accessor :uri
-  attr_accessible :value, :uri
+  attr_protected :property_id
 
   validates :uri, presence: true, url: true
 
   embedded_in :function
 
-  before_save :set_property_id
-
-
   private
 
-    def set_property_id
-      self.property_id = Addressable::URI.parse(uri).basename
-    end
+  def property_id=(property_id)
+    write_attribute :property_id, find_id(uri)
+  end
 end
