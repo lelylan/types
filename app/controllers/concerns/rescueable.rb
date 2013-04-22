d2ad8840b18300c8c06ba292427c148dcdd4aaf1
Andrea Reginato <andrea.reginato@gmail.com>
2013-04-22 15:02:02 +0200

    Refactoring controller concerns into a unique file

--- app/controllers/concerns/rescueable.rb
+++ app/controllers/concerns/rescueable.rb
@@ -19,4 +19,22 @@ module Rescueable
     code = 'notifications.json.not_valid'
     render_422 code, I18n.t(code)
   end
+
+
+  def render_401
+    self.class.serialization_scope :request
+    render 'show', status: 401, json: {}, serializer: ::NotAuthorizedSerializer and return
+  end
+
+  def render_404(code = 'notifications.resource.not_found', uri = nil)
+    self.class.serialization_scope :request
+    resource = { code: code, description: I18n.t(code), uri: (uri || request.url) }
+    render json: resource, status: 404, serializer: ::NotFoundSerializer and return
+  end
+
+  def render_422(code, description)
+    self.class.serialization_scope :request
+    resource = { code: code, description: description, body: request.request_parameters }
+    render json: resource, status: 422, serializer: ::NotValidSerializer and return
+  end
 end
